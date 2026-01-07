a1aee2251469731120aa37650e17514dfb71933b
lets00 <eduardovansilva@gmail.com>
2026-01-07 11:06:10 -0300

    fix: quantidade de filmes que o teste espera

--- src/tests/movies.test.js
+++ src/tests/movies.test.js
@@ -5,7 +5,7 @@ describe('Teste do endpoint /filmes', () => {
     it('Deve ser capaz de obter todos os filmes adicionados no array', async () => {
         const resultado = await request(app).get('/api/filmes')
         expect(resultado.status).toBe(200)
-        expect(resultado.body.length).toBe(6)
+        expect(resultado.body.length).toBe(8)
         expect(resultado.body == null).toBe(false)
     })
 })
